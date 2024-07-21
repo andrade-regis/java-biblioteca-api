1ce8a0d2b1f20d6c275948ee962fef53d90bfedb
Regis Andrade <regis.batista.andrade@outlook.com>
2024-07-20 22:13:23 -0300

    Remoção de projeto por erro

--- src/main/java/Biblio_API/repository/BookRepository.java
+++
@@ -1,8 +0,0 @@
-package Biblio_API.repository;
-
-import Biblio_API.model.Book;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BookRepository extends JpaRepository<Book, Long> {
-}
-
